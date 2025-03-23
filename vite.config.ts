92891e5294cdc59ee9b99309251e550786882614
Greg Zancewicz <greg.zancewicz@gmail.com>
2025-03-23 09:20:06 -0500

    First successful launch

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/cesium/Build/Cesium/Workers/*',
-          dest: 'Workers',
-        },
-        {
-          src: 'node_modules/cesium/Build/Cesium/ThirdParty/*',
-          dest: 'ThirdParty',
-        },
-        {
-          src: 'node_modules/cesium/Build/Cesium/Assets/*',
-          dest: 'Assets',
-        },
-        {
-          src: 'node_modules/cesium/Build/Cesium/Widgets/*',
-          dest: 'Widgets',
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
